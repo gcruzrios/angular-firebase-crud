31d5dbf0bef63d83c1a14f2f76f5344c2e520db1
gcruzrios <gcruzr@gmail.com>
2021-04-18 19:30:36 -0600

    Crud corregido con cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,27 @@
 
 export const environment = {
   production: false,
+  // firebase: {
+  //   apiKey: "AIzaSyDIhG12JgNRrnJI9s3Z4BN3zIyT7evLHRE",
+  //   authDomain: "empleados-bccc9.firebaseapp.com",
+  //   databaseURL: "https://empleados-bccc9.firebaseio.com",
+  //   projectId: "empleados-bccc9",
+  //   storageBucket: "empleados-bccc9.appspot.com",
+  //   messagingSenderId: "798540677359",
+  //   appId: "1:798540677359:web:2965abdd8a76f72db09b1a"
+  // }
+
+// Your web app's Firebase configuration
   firebase: {
-    apiKey: "AIzaSyDIhG12JgNRrnJI9s3Z4BN3zIyT7evLHRE",
-    authDomain: "empleados-bccc9.firebaseapp.com",
-    databaseURL: "https://empleados-bccc9.firebaseio.com",
-    projectId: "empleados-bccc9",
-    storageBucket: "empleados-bccc9.appspot.com",
-    messagingSenderId: "798540677359",
-    appId: "1:798540677359:web:2965abdd8a76f72db09b1a"
+    apiKey: "AIzaSyDBoJfLTlRhe8eGtdpBqCyaD1cc6fKTkvk",
+    authDomain: "angular-empleados-3d797.firebaseapp.com",
+    projectId: "angular-empleados-3d797",
+    storageBucket: "angular-empleados-3d797.appspot.com",
+    messagingSenderId: "787450255521",
+    appId: "1:787450255521:web:e0dfcd7e47a1ecb884b8e0"
   }
+
+
 };
 
 /*
